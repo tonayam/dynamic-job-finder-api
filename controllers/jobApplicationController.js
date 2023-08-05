cccf2b3b60f24017e478e500fc12f6950998c5bd
tonayam <tonayam99@gmail.com>
2023-08-05 13:39:45 +0100

    Updates

--- controllers/jobApplicationController.js
+++ controllers/jobApplicationController.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require(`http-status-codes`);
 const checkPermissions = require(`../utils/checkPermissions`);
 const cloudinary = require(`cloudinary`).v2;
 const fs = require(`fs`);
+const path = require('path');
 
 // CREATE JOB APPLICATION (USER)
 const createJobApplication = async (req, res) => {
@@ -27,7 +28,9 @@ const createJobApplication = async (req, res) => {
 const getAllJobApplications = async (req, res) => {
   const appliedJobs = await JobApplication.find({
     createdBy: req.user.userId,
-  }).sort({ createdAt: -1 });
+  })
+    .sort({ createdAt: -1 })
+    .populate({ path: `job`, select: `jobTitle` });
 
   res.status(StatusCodes.OK).json({ appliedJobs, count: appliedJobs.length });
 };
